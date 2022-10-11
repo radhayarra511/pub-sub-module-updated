3fcd16297e91fd0ac77c8b6e4694ef2dc209686a
yarraradha <43906675+yarraradha@users.noreply.github.com>
2022-10-11 23:39:00 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,63 @@
+variable "project_id_topic" {
+  type        = string
+  description = "(Required) The project ID to manage the Pub/Sub topic"
+}
+
+variable "project_push_sub" {
+  type        = string
+  description = "(Required) The project ID to manage the Pub/Sub subscription"
+  default  = null
+}
+
+variable "project_pull_sub" {
+  type        = string
+  description = "(Required) The project ID to manage the Pub/Sub subscription"
+  default  = null
+}
+
+variable "topic_name" {
+  type        = string
+  description = "(Required) The Pub/Sub topic name"
+}
+
+variable "topic_labels" {
+  type        = map(string)
+  description = "A map of labels to assign to the Pub/Sub topic"
+  default     = {}
+}
+
+variable "push_subscriptions" {
+  type        = list(map(string))
+  description = "The list of the push subscriptions"
+  default     = []
+}
+
+variable "pull_subscriptions" {
+  type        = list(map(string))
+  description = "The list of the pull subscriptions"
+  default     = []
+}
+
+variable "subscription_labels" {
+  type        = map(string)
+  description = "A map of labels to assign to every Pub/Sub subscription"
+  default     = {}
+}
+
+variable "message_storage_policy" {
+  type        = map(any)
+  description = "A map of storage policies. Default - inherit from organization's Resource Location Restriction policy."
+  default     = {}
+}
+
+variable "schema_settings" {
+  type        = map(any)
+  description = "Settings for validating messages published against a schema."
+  default     = null
+}
+
+variable "kms_key_name" {
+  type        = string
+  description = "The resource name of the Cloud KMS CryptoKey to be used to protect access to messages published on this topic."
+  default     = null
+}
